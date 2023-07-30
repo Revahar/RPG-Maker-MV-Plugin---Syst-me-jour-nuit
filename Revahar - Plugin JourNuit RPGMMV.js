d9d346693133091eb970ca4413e6548fcd3e58b9
Michaud Nathanael <nath.michaud@hotmail.fr>
2023-07-30 20:12:47 +0200

    Update - système jourNuit
    Ajout de la gestion des teintes par l'utilisateur

--- Revahar - Plugin JourNuit RPGMMV.js
+++ Revahar - Plugin JourNuit RPGMMV.js
@@ -1,16 +1,15 @@
 //=============================================================================
-// Revahar - Plugin Jour/Nuit RPGMMV
+// Revahar - Plugin JourNuit RPGMMV
 //=============================================================================
-
 /*:
- * @plugindesc Plugin de système jour/nuit pour RPG Maker MV.
- * Version 1.0
+ * @plugindesc Plugin de système jour/nuit pour RPG Maker MV. test de fonctionnement
+ * Version 1.1
  * @author Revahar
- * 
+ *
  * @param Variable Matin
  * @desc La variable contenant l'heure du début du matin
  * @default 1
- * 
+ *
  * @param Variable Journee
  * @desc La variable contenant l'heure du début de la journée
  * @default 2
@@ -18,7 +17,7 @@
  * @param Variable Soir
  * @desc La variable contenant l'heure du début du soir
  * @default 3
- * 
+ *
  * @param Variable Nuit
  * @desc La variable contenant l'heure du début de la nuit
  * @default 4
@@ -46,29 +45,53 @@
  * @param Transition Frames
  * @desc Le nombre de frames pour la transition entre chaque teinte.
  * @default 600
- * 
+ *
+ * @param Frame On Exit
+ * @desc Le nombre de frames pour la transition de teinte lors de la sortie d'un lieu.
+ * @default 0
+ *
  * @param Switch Matin
  * @desc Le switch correspondant à la période du matin
  * @default 1
- * 
+ *
  * @param Switch Journee
  * @desc Le switch correspondant à la période de la journée
  * @default 2
- * 
+ *
  * @param Switch Soir
  * @desc Le switch correspondant à la période du soir
  * @default 3
- * 
+ *
  * @param Switch Nuit
  * @desc Le switch correspondant à la période de la nuit
  * @default 4
  *
- * @help Ce plugin permet d'ajouter un système jour/nuit au jeu. 
- * Une journée est divisée en 4 phases. 
+ * @param TeinteDongeon
+ * @desc La teinte souhaitée pour l'intérieur des dongeons (format: [R, G, B, Gray])
+ * @default [-200, -200, -200, 0]
+ *
+ * @param TeinteMatin
+ * @desc La teinte souhaitée pour le matin (format: [R, G, B, Gray])
+ * @default [-16, -84, -118, 120]
+ *
+ * @param TeinteJournee
+ * @desc La teinte souhaitée pour la journée (format: [R, G, B, Gray])
+ * @default [0, 0, 0, 0]
+ *
+ * @param TeinteSoir
+ * @desc La teinte souhaitée pour le soir (format: [R, G, B, Gray])
+ * @default [-42, -134, -134, 100]
+ *
+ * @param TeinteNuit
+ * @desc La teinte souhaitée pour la nuit (format: [R, G, B, Gray])
+ * @default [-200, -200, -100, 68]
+ *
+ * @help Ce plugin permet d'ajouter un système jour/nuit au jeu.
+ * Une journée est divisée en 4 phases.
  * La teinte de l'écran change en fonction de l'heure.
- * Les heures, minutes et secondes se trouvent dans les varibales du jeu 
+ * Les heures, minutes et secondes se trouvent dans les variables du jeu
  * (respectivement 1, 2 et 3 par défaut).
- * Servez vous en pour les afficher ou pour définir des heures d'ouverture 
+ * Servez-vous en pour les afficher ou pour définir des heures d'ouverture
  * pour les magasins par exemple.
  *
  * Pour désactiver les teintes, utilisez la commande de plugin suivante :
@@ -78,36 +101,35 @@
  * EnableTint
  * /!\ Chaque Map où vous souhaitez que le système jour/nuit soit actif doit
  * contenir un EnableTint.
- * 
+ *
  * Pour que la teinte se mette à jour instantanément lors de la sortie d'un
  * lieu où la gestion des teintes était désactivée, utilisez la commande
- * de plugin : EnableTintOnExit
- * /!\ À mettre dans l'évènement de la porte de sortie du bâtiment juste avant
+ * de plugin suivante : EnableTintOnExit
+ * /!\ A mettre dans l'événement de la porte de sortie du bâtiment juste avant
  * la téléportation.
- * 
- *  Pour définir la teinte dans un dongeon (modifiable à la ligne 239)
- *  utilisez la commande de plugin : DungeonTint
- * /!\ À mettre dans un évènement parallèle.
  *
- * Pour ajouter manuellement des heures, utilisez la commande de plugin 
+ * Pour définir la teinte dans un donjon (modifiable à la ligne 239)
+ * utilisez la commande de plugin suivante : DungeonTint
+ * /!\ A mettre dans un événement parallèle.
+ *
+ * Pour ajouter manuellement des heures, utilisez la commande de plugin
  * suivante : AddHours x
  * Remplacez x par le nombre d'heures à ajouter.
- * Par exemple, si il est 23h en jeu et que vous ajoutez 8h, il sera 7h.
- * 
- * Par défaut, les switch 1, 2, 3 et 4 sont utilisées pour connaitre la
- * période en cours (matin, journée, soir, nuit). Vous pouvez les modifier 
- * selon vos switches dans les paramètres.
- * Cela permet, via une seule condition (Si variable 4 = ON alors ...) de
- * permettre un évènement en fonction de la période du jour (par exemple
- * un magasin qui ne serait ouvert que la journée, un donjon accessible que
- * la nuit, etc.) 
+ * Par exemple, s'il est 23h dans le jeu et que vous ajoutez 8h, il sera 7h.
  *
- * Les teintes se mettent à jour automatiquement en fonction
- * de l'heure en jeu.
+ * Par défaut, les interrupteurs 1, 2, 3 et 4 sont utilisés pour connaître la
+ * période en cours (matin, journée, soir, nuit). Vous pouvez les changer
+ * en fonction de vos interrupteurs dans les paramètres.
+ * Cela permet, via une seule condition (Si interrupteur 4 = ON alors...), de
+ * permettre un événement en fonction de l'heure (par exemple, un magasin qui
+ * ne serait ouvert que le jour, un donjon accessible seulement la nuit, etc.).
+ *
+ * Les teintes se mettent à jour automatiquement en fonction de l'heure du jeu
+ * sur une durée définie en frames dans le paramètre "Frames par seconde".
  */
 
 (function() {
-    var parameters = PluginManager.parameters('JourNuit');
+    var parameters = PluginManager.parameters('Revahar - Plugin JourNuit RPGMMV');
     var varMatin = parseInt(parameters['Variable Matin']);
     var varJournee = parseInt(parameters['Variable Journee']);
     var varSoir = parseInt(parameters['Variable Soir']);
@@ -117,11 +139,12 @@
     var soirHeure = parseInt(parameters['Soir Heure']);
     var nuitHeure = parseInt(parameters['Nuit Heure']);
     var switchMatin = parseInt(parameters['Switch Matin']);
-    var switchJournee = parseInt(parameters['Switch Journée']);
+    var switchJournee = parseInt(parameters['Switch Journee']);
     var switchSoir = parseInt(parameters['Switch Soir']);
     var switchNuit = parseInt(parameters['Switch Nuit']);
     var heureFrames = parseInt(parameters['Frames par seconde']);
     var transitionFrames = parseInt(parameters['Transition Frames']);
+    var frameOnExit = parseInt(parameters['Frame On Exit']);
     var tintEnabled = true;
     var tintOnExit = false;
     var dungeonTintValue = false;
@@ -129,37 +152,37 @@
     var targetTint = null;
     var frameCount = 0;
 
-    // Désactiver les teintes
+    // Turn off tints
     function disableTint() {
         tintEnabled = false;
         updateTint();
     }
 
-    // Réactiver les teintes
+    // Reactivate tints
     function enableTint() {
-        transitionFrames = parseInt(parameters['Transition Frames']);;
+        transitionFrames = parseInt(parameters['Transition Frames']);
         tintEnabled = true;
         updateTint();
     }
 
-    // Réactiver les teintes en sortie de bâtiment/donjon
+    // Reactivate colors when exiting buildings/dungeons
     function enableTintOnExit() {
         tintOnExit = true;
         updateTint();
     }
 
-    // Définir une teinte pour les donjons/cavernes/grottes
+    // Set tint for dungeons/caves
     function dungeonTint() {
         dungeonTintValue = true;
         updateTint();
     }
 
-    // Ajouter des heures
+    // Add hours
     function addHours(hours) {
         var currentTime = $gameVariables.value(1); // Variable contenant l'heure
         var newTime = currentTime + hours;
 
-        // Gérer le dépassement de minuit
+        // Manage passing midnight
         if (newTime >= 24) {
             newTime -= 24;
         } else if (newTime < 0) {
@@ -170,20 +193,15 @@
         updateTint();
     }
 
-    
+    // Update tint
+    function updateTint() {
+        var currentTime = $gameVariables.value(1); // Variable containing the time
+        var periodeMatin = $gameSwitches.value(switchMatin); // Switch determining the morning period (OFF = not morning, ON = morning)
+        var periodeJournee = $gameSwitches.value(switchJournee); // Switch determining the day period (OFF = no day, ON = day)
+        var periodeSoir = $gameSwitches.value(switchSoir); // Switch determining the evening period (OFF = no evening, ON = evening)
+        var periodeNuit = $gameSwitches.value(switchNuit); // Switch determining the night period (OFF = no night, ON = night)
 
-// Mettre à jour la teinte
-function updateTint() {
- 
-
-        var currentTime = $gameVariables.value(1); // Variable contenant l'heure
-        var periodeMatin = $gameSwitches.value(switchMatin); // Switch déterminant la période matin (OFF = pas matin, ON = matin)
-        var periodeJournee = $gameSwitches.value(switchJournee); // Switch déterminant la période jour (OFF = pas jour, ON = jour)
-        var periodeSoir = $gameSwitches.value(switchSoir); // Switch déterminant la période soir (OFF = pas soir, ON = soir)
-        var periodeNuit = $gameSwitches.value(switchNuit); // Switch déterminant la période nuit (OFF = pas nuit, ON = nuit)
-
-
-        // Déterminer la phase actuelle en fonction de l'heure
+        // Determine current phase based on time
         var phase = 'matin';
         if (currentTime >= matinHeure && currentTime < journeeHeure) {
             phase = 'matin';
@@ -216,38 +234,34 @@ function updateTint() {
         $gameSwitches.setValue(varSoir, periodeSoir);
         $gameSwitches.setValue(varNuit, periodeNuit);
 
-
-
-
-        // Obtenir la teinte correspondante à la phase
+        // Obtain the tint corresponding to the phase
         var targetTint = getTintByPhase(phase);
 
-        // Changer la teinte en sortie de bâtiment/donjon
-        // modifiez transitionFrames en fonction de la durée de la transition souhaité (exemple : 60 frames = 1 seconde)
+        // Change tint when exiting building/dungeon
         if (tintOnExit) {
-            currentTint = targetTint
-            transitionFrames = 0; //<- remplacez le "0" par le nombre de frames souhaité
+            currentTint = targetTint;
+            transitionFrames = frameOnExit;
             $gameScreen.startTint(currentTint, transitionFrames);
             tintOnExit = false;
             return;
         }
 
-        // défini la teinte des donjons/grottes/etc.
+        // Set the tint of dungeons/caves/etc.
         if (dungeonTintValue) {
-            currentTint = [-200, -200, -200, 0];  //<- modifier ici les valeurs pour obtenir la teinte souhaitée
+            currentTint = JSON.parse(parameters['TeinteDongeon']);
             $gameScreen.startTint(currentTint, 0);
             dungeonTintValue = false;
             return;
-        } 
+        }
 
-        // Désactive la teinte si tintEnabled est = false
+        // Disable tint if tintEnabled is false
         if (!tintEnabled) {
             currentTint = [0, 0, 0, 0];
             $gameScreen.startTint(currentTint, 0);
             return;
-        } 
+        }
 
-        // Mettre à jour la teinte de façon progressive
+        // Gradually update the tint
         if (currentTint === null) {
             currentTint = targetTint;
             $gameScreen.startTint(currentTint, transitionFrames);
@@ -257,7 +271,7 @@ function updateTint() {
         }
     }
 
-    // Vérifier si deux teintes sont identiques
+    // Check if two shades are the same
     function tintEquals(tint1, tint2) {
         return (
             tint1[0] === tint2[0] &&
@@ -267,36 +281,36 @@ function updateTint() {
         );
     }
 
-    // Obtenir la teinte en fonction de la phase
+    // Get tint based on phase
     function getTintByPhase(phase) {
         switch (phase) {
             case 'matin':
-                return [-16, -84, -118, 120];
+                return JSON.parse(parameters['TeinteMatin']);
             case 'journee':
-                return [0, 0, 0, 0];
+                return JSON.parse(parameters['TeinteJournee']);
             case 'soir':
-                return [-42, -134, -134, 100];
+                return JSON.parse(parameters['TeinteSoir']);
             case 'nuit':
-                return [-200, -200, -100, 68];
+                return JSON.parse(parameters['TeinteNuit']);
             default:
                 return [0, 0, 0, 0];
         }
     }
 
-    // Mettre à jour la teinte à chaque frame
+    // Update tint every frame
     var _Scene_Map_update = Scene_Map.prototype.update;
     Scene_Map.prototype.update = function() {
         _Scene_Map_update.call(this);
 
         frameCount++;
         if (frameCount % heureFrames === 0) {
-            var currentTime = $gameVariables.value(1); // Variable contenant l'heure
-            var currentMinutes = $gameVariables.value(2); // Variable contenant les minutes
-            var currentSeconds = $gameVariables.value(3); // Variable contenant les secondes
+            var currentTime = $gameVariables.value(1); // Variable containing hours
+            var currentMinutes = $gameVariables.value(2); // Variable containing the minutes
+            var currentSeconds = $gameVariables.value(3); // Variable containing the seconds
 
             currentSeconds += 1;
 
-            // Gérer les dépassements des secondes et des minutes
+            // Manage overruns of seconds and minutes
             if (currentSeconds >= 60) {
                 currentSeconds = 0;
                 currentMinutes ++;
@@ -318,7 +332,7 @@ function updateTint() {
         }
     };
 
-    // Gestion des commandes de plugin
+    // Plugin command Management
     var _Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
     Game_Interpreter.prototype.pluginCommand = function(command, args) {
         _Game_Interpreter_pluginCommand.call(this, command, args);
@@ -327,9 +341,9 @@ function updateTint() {
             disableTint();
         } else if (command === 'EnableTint') {
             enableTint();
-        } else if (command === 'EnableTintOnExit'){
+        } else if (command === 'EnableTintOnExit') {
             enableTintOnExit();
-        } else if (command === 'DungeonTint'){
+        } else if (command === 'DungeonTint') {
             dungeonTint();
         } else if (command === 'AddHours') {
             var hours = parseInt(args[0]);
